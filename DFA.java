3bbaef28d307e2e9f2df9e53a830d06477d46b66
Carl Kevin Galvan <carl2.galvan@neu.edu.ph>
2022-09-12 22:38:34 -0700

    Assignment #2_DFA to java code

---
+++ DFA.java
@@ -0,0 +1,48 @@
+package theoryOfProgramming;
+public class DFA 
+{
+    public static void main(String[] args)
+    {
+        String state = "q0";
+		String input = "1000101";		
+		for(int i=0; i<input.length();i++)
+		{
+			if(state.equals("q0") && input.charAt(i)=='1')
+			{
+				state = "q0";
+			}
+			else if(state.equals("q0") && input.charAt(i)=='0')
+			{
+				state = "q1";
+			}
+			else if(state.equals("q1") && input.charAt(i)=='0')
+			{
+				state = "q1";
+			}
+			else if(state.equals("q1") && input.charAt(i)=='1')
+			{
+				state = "q2";
+			}
+			else if(state.equals("q2") && input.charAt(i)=='0')
+			{
+				state = "q1";
+			}
+			else if(state.equals("q2") && input.charAt(i)=='1')
+			{
+				state = "q0";
+			}
+			else
+			{
+				System.out.println("Invalid input.. Existing automaton");
+				break;
+			}
+		}		
+		if(state.equals("q2")) {
+			System.out.println("String accepted!");
+		}
+		else {
+			System.out.println("String not accepted!");
+		}
+    }
+    
+}
