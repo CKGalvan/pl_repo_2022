71a49202cbd663169a24cd4a2442e9691ae4c001
CKevinGalvan <galvancarlkevin@gmail.com>
2022-10-23 23:21:41 -0700

    SyntaxAnalyzer

---
+++ SyntaxAnalysis.java
@@ -0,0 +1,28 @@
+package theoryOfProgramming;
+import java.util.ArrayList;
+
+class SyntaxAnalysis
+{
+    public static void main(String[] args)
+    {   
+        String output1 = "<data_type> <identifier> <assignment_operator> <delimiter> <value> ";
+        String output = "<data_type> <identifier> <assignment_operator> <value> <delimiter>";
+        System.out.println(analysingSyntax(output));
+    }
+    static String analysingSyntax(String output)
+    {
+        String str = "<data_type><identifier><assignment_operator><value><delimiter>";
+        String str1 = "<data_type><identifier><delimiter>";
+        output = output.replace(" ", "");
+        String ver = "";
+        if(str.equals(output) || str1.equals(output))
+        {
+            ver = "Syntax is Correct!";
+        }
+        else if(!str.equals(output) || !str1.equals(output))
+        {
+            ver="Syntax is Error!";
+        }
+        return ver;
+    }
+}
