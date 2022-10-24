13774bbb7eb1a6723abea9437a806d4dba82349e
Carl Kevin Galvan <113296909+CKGalvan@users.noreply.github.com>
2022-10-24 04:53:04 -0700

    Updated Tokenizer

--- Tokenizer.java
+++ Tokenizer.java
@@ -1,22 +1,24 @@
 package theoryOfProgramming;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 public class Tokenizer 
 {
-    static String strString = "";
-	static ArrayList<String> output = new ArrayList<String>();
-    static ArrayList<String> output1 = new ArrayList<String>();
-	static ArrayList<String> dataType = new ArrayList<String>();
-	static ArrayList<String> operator = new ArrayList<String>();
-	static String delimeter = ";";
-	static char quoteForString= '"';
-	public static void identify(String str)
-	{
-		String str1="";
-        int strLength = str.length();
-		int count =0,count1=0;
-        boolean stringQuoteOpen = false;    
-		for(int x=0;x<strLength;x++)//
+	static String identifyingLexeme(String str)//carl kevin galvan
+    {
+        String strString = "",str1="",delimeter = ";";
+	    ArrayList<String> output = new ArrayList<String>();
+        ArrayList<String> output1 = new ArrayList<String>();
+        String[] dataType1 = {"String", "int", "double", "char", "float"};
+		String[] operator1  = {"=", "+", "-"};
+        int strLength = str.length();;
+		int count=0,count1=0;
+        boolean stringQuoteOpen = false; 
+
+        List dataType = Arrays.asList(dataType1);
+        List operator = Arrays.asList(operator1);  
+		for(int x=0;x<strLength;x++)//=
 		{                             
 			//identification of the dataType
 			strString = Character.toString(str.charAt(x));    
@@ -70,14 +72,14 @@ public class Tokenizer
             else if(((Character.isDigit(str.charAt(x))||str.charAt(x)=='.') && (stringQuoteOpen==false)))
             {
                 str1+=strString;                
-                if(Character.isDigit(str.charAt(x+1))==false)
+                if(Character.isDigit(str.charAt(x+1))==false && str.charAt(x+1)!='.')
                 {
                     output.add(str1);
                     output1.add("<value>");
                     str1="";
                 }
             }
-             //-------------------------------------------------------     
+             //------------------------------------------------------------------
             else if((Character.isLetter(str.charAt(x))==true) && (stringQuoteOpen==false))
             {               
                 str1+=strString;
@@ -103,26 +105,21 @@ public class Tokenizer
                 output1.add("<assignment_operator>");
             }                 
 		}
+        String result = output1.toString().replace(" ", "").replace("]", "").
+        replace("[", "").replace(",", "");
+        //return result;
+        System.out.println("output: "+output);
+        System.out.println(result);
+        return result;
 	}
 	public static void main(String[] args) 
-	{
-		String[] dataType1 = {"String", "int", "double", "char", "float"};
-		String[] operator1  = {"=", "+", "-"};
-		for(int i=0;i<dataType1.length;i++)
-		{
-			dataType.add(dataType1[i]);
-		}	
-		for(int i=0; i<operator1.length;i++)
-		{
-			operator.add(operator1[i]);
-		}	
+	{	
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter Source Language: ");     
 		String str = scan.nextLine();
-		identify(str);	
-		//output.forEach(output ->{System.out.print(output);});	
+        String lexemes=identifyingLexeme(str);
         System.out.println();
-        System.out.print("Output is: ");
-        output1.forEach(output1 ->{System.out.print(output1);});		
+        System.out.print("Output is: ");	
+        System.out.println(lexemes);
 	}   
 }
